a70fbc1a1fbe629eafb59ecfac6a64174c89dedc
Natalya <soltk7@yandex.ru>
2020-08-07 19:52:09 +0300

    add repo for psy-test site

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Лучшие тесты online
+	</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+</head>
+<body>
+<div class="content">
+<div class="header">
+	<?php  
+include "menu.php";
+?>	
+
+</div>
+
+
+
+<h1>Лучшие тесты online здесь!</h1>
+<div class="center">
+<img src="img/1.jpg">
+<div class="box_text">
+<p><b>Добрый день.</b> На нашем сайте вы можете пройти наилучшие профессиональные психологические тесты.
+
+<p>Также вы можете ознакомиться с современными методами психологического тестирования и скачать книги по данной тематике.</p>
+
+<p>Путеводитель по разделам сайта: <br>
+	
+		<a href="puzzle.php">Тесты</a>  <br>
+		<a href="guess.php">Статьи</a> <br>
+		<a href="multiguess.php">Библиотека</a>  <br>
+		<a href="parol.php">Статистика тестирований</a>  <br>
+
+
+</div>
+</div>
+</div>
+<div class="footer">Copyright &copy; <?php echo date("Y") ;?> Rikanda </div>
+
+</body>
+</html>
